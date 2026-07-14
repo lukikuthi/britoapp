a1b4993edfd97ad111610f6a5eba58770ab09457
lukikuthi <lucaskikuthi@gmail.com>
2026-07-14 13:41:28 -0300

    Deploy 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tanstackStart(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
